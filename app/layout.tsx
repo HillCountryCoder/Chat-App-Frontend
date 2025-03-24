1f6edf196a147be39e7962711b7d65055f80b4be
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-03-24 22:54:21 +0500

    Wrap application in QueryProvider and SocketProvider for enhanced data management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import QueryProvider from "@/providers/query-provider";
+import { SocketProvider } from "@/providers/socket-provider";
 import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>
+          <SocketProvider>{children}</SocketProvider>
+        </QueryProvider>
       </body>
     </html>
   );
