c193d7148c12de31b18c3444fa451a16f8b15164
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-04-28 13:08:13 +0500

    fix: add eslint directive to suppress no-explicit-any rule in use-unread hook

--- hooks/use-unread.ts
+++ hooks/use-unread.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // hooks/use-unread.ts
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "@/lib/api";
